c8028bf13387b06ac1c98a026d3b517cc45ccff7
diogocorreia01 <diogo.f.correia@protonmail.com>
2024-11-20 22:15:24 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aprendizagem_reforco',
+    version='1.0.0',
+    description='Modelo de Aprendizagem por Reforço desenvolvido para o projeto2 da cadeira de IASC',
+    author='Diogo Correia',
+    author_email='diogo.f.correia@protonmail.com',
+    url='https://github.com/diogocorreia01/aprendizagem_reforco.git',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    python_requires='>=3.12',
+)
